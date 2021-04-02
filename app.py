5fd8413999543fdf2a06d231313d695bee6c7ecb
Ciara Spencer <ciara.spencer314@gmail.com>
2021-04-02 12:26:26 -0400

    re-route

--- app.py
+++ app.py
@@ -10,8 +10,11 @@ def home():
 
 @app.route("/industry")
 def industry():
-    
     return render_template("industry.html")
+
+@app.route("/geo")
+def industry():
+    return render_template("geoindex.html")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
