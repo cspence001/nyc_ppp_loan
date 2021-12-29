190e7d5271ceb2cff2d764840e1d3f794a8705dc
Ciara Spencer <ciara.spencer314@gmail.com>
2021-12-29 16:29:05 -0500

    file update

--- static/js/geologic.js
+++ static/js/geologic.js
@@ -17,30 +17,27 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
   }).addTo(myMap);
 /////////
 
+d3.csv("/static/data/geo/nycppprecipients_geo.csv").then(function(data){
+  console.log(data);
+  // Create a new marker cluster group
+  var markers = L.markerClusterGroup();
 
-  // Grab the data with d3
-  d3.csv("/static/data/geo/nycppprecipients_geo.csv").then(function(data){
-    console.log(data);
-    // Create a new marker cluster group
-    var markers = L.markerClusterGroup();
+  // Loop through data
+  for (var i = 0; i < data.length; i++) {
+    // Set the latitude, longitude property to a variable
+    var latitude = data[i].lat;
+    var longitude = data[i].lng;
+    var popText = ("<h4>" + data[i].BusinessName + "</h4><h4>Address: " + data[i].Address+ "</h4>" + "<h4>Industry: " +data[i].Industry +"</h4>" + "<h4>LoanRange: " +data[i].LoanRange + "</h4><h4>JobsRetained: " + data[i].JobsRetained +"</h4>" );
+    // var industry = data[i].Industry;
+    // var mySelector = $("#Industry").val();
+    // Check for location property
 
-    // Loop through data
-    for (var i = 0; i < data.length; i++) {
-      // Set the latitude, longitude property to a variable
-      var latitude = data[i].lat;
-      var longitude = data[i].lng;
-      var popText = ("<h3>" + data[i].BusinessName + "</h3><h3>Address: " + data[i].Address+ "</h3>" + "<h3>Industry: " +data[i].Industry +"</h3>" + "<h3>LoanRange: " +data[i].LoanRange + "</h3><h3>JobsRetained: " + data[i].JobsRetained +"</h3>" );
-      // var industry = data[i].Industry;
-      // var mySelector = $("#Industry").val();
-      // Check for location property
-
-      if (latitude) {
-        // Add a new marker to the cluster group and bind a pop-up
-        markers.addLayer(L.marker([latitude, longitude])
-          .bindPopup(popText));
-        };
-    }
-    // Add our marker cluster layer to the map
-    myMap.addLayer(markers);
-  });
-
+    if (latitude) {
+      // Add a new marker to the cluster group and bind a pop-up
+      markers.addLayer(L.marker([latitude, longitude])
+        .bindPopup(popText));
+      };
+  }
+  // Add our marker cluster layer to the map
+  myMap.addLayer(markers);
+});
